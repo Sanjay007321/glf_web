bb54f0df967f63652a56d2f2ab8831c100e2bced
Sanjay007321 <saniay007aj@gmail.com>
2025-09-18 12:18:52 +0530

    Update Vision.jsx

--- frontend/src/components/Vision.jsx
+++ frontend/src/components/Vision.jsx
@@ -5,9 +5,9 @@ return(
 <h1 className="text-2xl mx-auto bg-gradient-to-r from-yellow-200 via-amber-300 to-yellow-400 bg-clip-text text-transparent mt-2">Visions Of GLF</h1>
 <div className="flex m-3 hover:shadow-md duration-900 ease-in-out shadow-cyan-300 bg-gray-800 border flex-col border-green-300 rounded-4xl p-3 gap-4">
 <div className="flex flex-wrap gap-y-5 gap-x-4 mt-5">
-<img src="src/assets/WhatsApp Image 2025-08-09 at 2.06.13 PM (2).jpeg" className="border-5 border-amber-200 md:h-50 md:w-50 h-35 rounded-2xl ml-auto"></img>
-<img src="src/assets/WhatsApp Image 2025-08-09 at 2.06.15 PM.jpeg" className="border-5 border-amber-200 md:h-50 md:w-50 h-35 rounded-2xl mr-auto md:ml-auto"></img>
-<img src="src/assets/WhatsApp Image 2025-08-09 at 2.06.17 PM (1).jpeg" className="border-5 border-amber-200 md:h-50  h-35 rounded-2xl mx-auto"></img>
+<img src="WhatsApp Image 2025-08-09 at 2.06.13 PM (2).jpeg" className="border-5 border-amber-200 md:h-50 md:w-50 h-35 rounded-2xl ml-auto"></img>
+<img src="WhatsApp Image 2025-08-09 at 2.06.15 PM.jpeg" className="border-5 border-amber-200 md:h-50 md:w-50 h-35 rounded-2xl mr-auto md:ml-auto"></img>
+<img src="WhatsApp Image 2025-08-09 at 2.06.17 PM (1).jpeg" className="border-5 border-amber-200 md:h-50  h-35 rounded-2xl mx-auto"></img>
 </div>
 <p>➤ Ensuring food, clothing, shelter for the destitute elderly, women and children.</p>
 <p>➤ Providing self-employment opportunities to unemployed persons with disabilities.</p>
