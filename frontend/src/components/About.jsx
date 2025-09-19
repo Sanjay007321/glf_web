d68f0ded5085394f5137e6665a73314eefb6f4bf
Sanjay007321 <saniay007aj@gmail.com>
2025-09-19 21:54:15 +0530

    Update About.jsx

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -6,7 +6,7 @@ return(
 </div>
 <div className='md:flex-grow hover:shadow-md duration-900 ease-in-out shadow-cyan-300 flex-col text-cyan-200 bg-gray-800 flex m-3 border border-green-300 rounded-4xl md:p-3 md:text-xl relative justify-center items-center' id='about'> 
 <div className='flex m-3 rounded-4xl justify-center items-center md:p-3'>
-<img className='border-5 border-amber-200 md:h-50 md:w-100 h-40 w-50 rounded-4xl ms-2 opacity-70 brightness-100 contrast-100' src='WhatsApp Image 2025-08-09 at 2.05.48 PM.jpeg' ></img>
+<img className='border-5 border-amber-200 md:h-50 md:w-100 h-30 w-30 rounded-4xl ms-2 opacity-70 brightness-100 contrast-100' src='WhatsApp Image 2025-08-09 at 2.05.48 PM.jpeg' ></img>
 <p className='text-wrap p-3'>Good Life Foundation was launched on 10/04/2024 under the leadership of social worker G.vigneswaran. The Good Life Foundation continues to work to provide equal rights and justice to all people in our country. It helps people who are deprived of basic amenities in their daily lives</p>
 </div>
 <div className="flex m-3 rounded-4xl p-3 items-center gap-4">
@@ -17,14 +17,14 @@ by providing them with education and employment opportunities.
 </p>
 <img
 src="WhatsApp Image 2025-08-09 at 2.06.15 PM (2).jpeg"
-className="w-45 h-45 object-cover rounded-4xl md:h-55 md:w-55 opacity-90 brightness-90 contrast-100 border-5 border-amber-200"
+className="w-30 h-30 object-cover rounded-4xl md:h-55 md:w-55 opacity-90 brightness-90 contrast-100 border-5 border-amber-200"
 alt="Founder"
 />
 </div>
 <div className="flex m-3  rounded-4xl p-3 items-center gap-4">
 <img
 src="WhatsApp Image 2025-08-09 at 2.06.15 PM (1).jpeg"
-className="border-5 border-amber-200 w-45 h-45 object-cover rounded-4xl md:h-55 md:w-55 opacity-90 brightness-90 contrast-100"
+className="border-5 border-amber-200 w-30 h-30 object-cover rounded-4xl md:h-55 md:w-55 opacity-90 brightness-90 contrast-100"
 alt="Founder"
 />
 <p className="flex-1 text-wrap p-1">The Secretary, Social worker Surya(MSW), through the executive committee of the organization, is taking strong action against drugs that pollute the education of rural people, social justice and equality.</p>
